ce593c7293431467cc3f99556b5864f2ff0453c6
Peter Lamson <ptrlmsn@gmail.com>
2021-11-02 09:07:00 -0400

    commit tree de9344661cc7f06e63fb7d0d7597cfb76ec7ac74

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+    var stageOne = `${customerName}`.toUpperCase()
+    customerName = stageOne
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'billy'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy two'
+}
